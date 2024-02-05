8885dad9e4ffdc892242a89f5e6e1e7d0233dc51
Mark Williams Owusu <markwilliamsowusu@gmail.com>
2024-02-05 12:01:07 +0000

    image url editted

--- backend/index.js
+++ backend/index.js
@@ -304,7 +304,7 @@ const storage = multer.diskStorage({
   app.post('/upload', upload.single('product'), (req, res) => {
     res.json({
       success: 1,
-      image_url: `http://localhost:${port}/images/${req.file.filename}`,
+      image_url: `https://e-commerce-api-xi.vercel.app/images/${req.file.filename}`,
     });
   });
   
